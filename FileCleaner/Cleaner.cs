0a70cc18b7348fb5d16619d0804d2cb9f67590b5
max_on <maks_on_max@mail.ru>
2024-07-07 14:53:36 +0700

    Добавьте файлы проекта.

---
+++ FileCleaner/Cleaner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCleaner
+{
+  internal class Cleaner
+  {
+    public void Clean(string _path)
+    {
+      try
+      {
+        DirectoryInfo dir = new DirectoryInfo(_path);
+
+        foreach (var subdir in dir.GetDirectories())
+        {
+          if ((DateTime.Now - subdir.LastWriteTime) > (DateTime.Now - DateTime.Now.AddMinutes(-30)))
+          {
+            Console.WriteLine($"Папка {subdir.Name} использовалась более 30 минут назад --> удаляем");
+            subdir.Delete(true);
+          }
+          else
+          {
+            Console.WriteLine($"Папка {subdir.Name} использовалась менее 30 минут назад --> пусть живет");
+          }
+        }
+
+        foreach (var file in dir.GetFiles())
+        {
+          if ((DateTime.Now - file.LastWriteTime) > (DateTime.Now - DateTime.Now.AddMinutes(-30)))
+          {
+            Console.WriteLine($"Файл {file.Name} использовался более 30 минут назад --> удаляем");
+            file.Delete();
+          }
+          else
+          {
+            Console.WriteLine($"Файл {file.Name} использовался менее 30 минут назад --> пусть живет");
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.ToString());
+      }
+    }
+  }
+}
---
+++ FileCleaner/Program.cs
@@ -0,0 +1,30 @@
+﻿using FileCleaner;
+
+internal class Program
+{
+  private static bool p_isCorrect;
+  private static Cleaner p_cleaner = new();
+  private static void Main(string[] args)
+  {
+    while (!p_isCorrect)
+    {
+      Console.Write($"Введите путь до папки: ");
+
+      string? path = Console.ReadLine();
+
+      if (string.IsNullOrWhiteSpace(path))
+      {
+        Console.WriteLine($"Путь некорректный");
+      }
+      else if (!Directory.Exists(path))
+      {
+        Console.WriteLine($"Указанного каталога не существует!");
+      }
+      else
+      {
+        p_cleaner.Clean(path);
+        p_isCorrect = true;
+      }
+    }
+  }
+}
